0621bb76c2c0f91045d7f8847ede5bb2fecf8ecb
Giles Knap <giles.knap@diamond.ac.uk>
2021-09-16 14:51:16 +0000

    get profile loading to work

--- dls/dev-centos/run-dev.sh
+++ dls/dev-centos/run-dev.sh
@@ -3,19 +3,17 @@
 mkdir -p /scratch/${USER}/work
 
 image=dev-c7
-path="-e PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/epics/epics-base/bin/linux-x86_64:/dls_sw/prod/tools/RHEL7-x86_64/defaults/bin:/home/${USER}/bin"
-environ="-e DISPLAY -e DLS_EPICS_RELEASE -e EPICS_EXTENSIONS -e EPICS_BASE \
-        -e EPICS_HOST_ARCH -e EPICS_CA_MAX_ARRAY_BYTES \
-        -e HOME=/home/${USER}"
+environ="-e HOME=/home/${USER}"
 volumes="-v /dls_sw/prod:/dls_sw/prod \
         -v /dls_sw/work:/dls_sw/work \
         -v /dls_sw/epics:/dls_sw/epics \
+        -v /dls_sw/apps:/dls_sw/apps \
         -v /dls_sw/etc:/dls_sw/etc -v /scratch/${USER}/work:/local \
         -v /home/${USER}:/home/${USER}"
 devices="-v /dev/ttyS0:/dev/ttyS0"
 opts="--net=host --rm -ti"
 x11opts="-v /dev/dri:/dev/dri --security-opt=label=type:container_runtime_t"
+# -l loads profile and bashrc
+command="/bin/bash -l"
 
-set -x
-podman run ${path} ${environ} ${x11opts} ${volumes} ${devices} ${@} ${opts} ${image}
-
+podman run ${environ} ${x11opts} ${volumes} ${devices} ${@} ${opts} ${image} ${command}
