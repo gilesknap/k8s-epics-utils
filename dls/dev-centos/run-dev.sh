bd8741f2f69f8f0432608da35314be1418f4e3c0
Giles Knap <giles.knap@diamond.ac.uk>
2021-09-17 09:32:24 +0100

    Get QT4 working

--- dls/dev-centos/run-dev.sh
+++ dls/dev-centos/run-dev.sh
@@ -3,16 +3,18 @@
 mkdir -p /scratch/${USER}/work
 
 image=dev-c7
-environ="-e HOME=/home/${USER}"
+environ="-e DISPLAY -e HOME -e WORKON_HOME=/scratch/${USER}/pipenv"
 volumes="-v /dls_sw/prod:/dls_sw/prod \
         -v /dls_sw/work:/dls_sw/work \
         -v /dls_sw/epics:/dls_sw/epics \
         -v /dls_sw/apps:/dls_sw/apps \
-        -v /dls_sw/etc:/dls_sw/etc -v /scratch/${USER}/work:/local \
-        -v /home/${USER}:/home/${USER}"
+        -v /dls_sw/etc:/dls_sw/etc \
+        -v /scratch:/scratch \
+        -v ${HOME}:${HOME}"
 devices="-v /dev/ttyS0:/dev/ttyS0"
 opts="--net=host --rm -ti"
 x11opts="-v /dev/dri:/dev/dri --security-opt=label=type:container_runtime_t"
+
 # -l loads profile and bashrc
 command="/bin/bash -l"
 
