68f45fb88141cc0fa20297c338712de35bf7f353
Giles Knap <giles.knap@diamond.ac.uk>
2022-02-07 09:58:21 +0000

    update Dockerfile to use RHEL 7 instead of centos 7

--- dls-images/dev-c7/build-dev.sh
+++ dls-images/dev-c7/build-dev.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-set -e
-podman build\
-    --network host\
-    -t gcr.io/diamond-pubreg/controls/dev-c7 .
+if [ -z "${1}" ] ; then
+    echo 'please supply a version tag as param 1 e.g. build-dev.sh 1.0'
+else
 
-module load gcloud
+    module load gcloud
 
-read -r -p "dev-c7 built OK. Push to gcr.io/diamond-pubreg [y/N] " response
-response=${response,,}    # tolower
-if [[ "$response" =~ ^(yes|y)$ ]] ; then
-    podman push gcr.io/diamond-pubreg/controls/dev-c7
+    set -e
+    podman build\
+        --network host\
+        -t gcr.io/diamond-pubreg/controls/dev-c7:${1} .
+
+    read -r -p "dev-c7 built OK. Push to gcr.io/diamond-pubreg [y/N] " response
+    response=${response,,}    # tolower
+    if [[ "$response" =~ ^(yes|y)$ ]] ; then
+        podman push gcr.io/diamond-pubreg/controls/dev-c7:${1}
+    fi
 fi
