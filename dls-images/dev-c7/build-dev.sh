78e495a1b2298b7bd493e8576171cc3becc88d5c
Giles Knap <giles.knap@diamond.ac.uk>
2022-03-29 08:24:20 +0100

    changes for dev-c7 1.2

--- dls-images/dev-c7/build-dev.sh
+++ dls-images/dev-c7/build-dev.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+name=gcr.io/diamond-pubreg/controls/dev-c7
+
+if [ -z ${1} ] ; then
+    echo usage: build-dev.sh '<new version number>'
+    echo look here to see the current versions http://${name}
+    exit 1
+fi
+
 set -e
 podman build\
     --network host\
-    -t gcr.io/diamond-pubreg/controls/dev-c7 .
+    -t ${name}:latest .
 
 module load gcloud
 
-read -r -p "dev-c7 built OK. Push to gcr.io/diamond-pubreg [y/N] " response
+read -r -p "dev-c7 built OK. Push to ${name}:${1} [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]] ; then
-    podman push gcr.io/diamond-pubreg/controls/dev-c7
+    podman tag ${name}:latest ${name}:${1}
+    podman push ${name}:latest
+    podman push ${name}:${1}
 fi
