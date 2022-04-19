16c26a66908d2e96970a873cb8753bf57e9fe076
Giles Knap <giles.knap@diamond.ac.uk>
2022-04-19 10:41:17 +0100

    switch devfunctions to use current git clone

--- dev-functions.sh
+++ dev-functions.sh
@@ -20,67 +20,86 @@ unset environ volumes devices opts identity
 environ="-e DISPLAY -e HOME"
 volumes="-v /tmp:/tmp -v ${HOME}:${HOME}"
 devices="-v /dev/ttyS0:/dev/ttyS0"
-opts="--net=host --privileged -ti --hostname cdev"
+opts="--net=host --privileged --hostname cdev"
 # identity="--security-opt=label=type:container_runtime_t"
 all_params="${environ} ${volumes} ${devices} ${opts} ${identity}"
 
 # utility functions ############################################################
 
+function get-image-name()
+{
+    # all cdev functions are intended to be run inside of a clone of 
+    # a container build project hosted on github.
+    #
+    # This function determines the github container registry name from
+    # the remote name and also generates a unique container name to use
+    # from the local root folder of the clone
+    
+    # extract remote git repo to determine ghcr image tag
+    image_repo=$(git remote -v 2> /dev/null | sed -e 's/.*github.com:\(.*\)\.git.*/\1/' -e 1q)
+    if [ -z "${image_repo}" ] ; then
+        echo "ERROR: This function must be run in a clone of a github container project"
+        return 1
+    fi
+    image_tag_base=ghcr.io/${image_repo}
+    image_tag=${image_tag_base}:work
+
+    # use root folder for container name but change / to _ and remove leading /
+    repo_root=$(git rev-parse --show-toplevel)
+    container_name=$(echo ${repo_root} | sed -e s=/=_=g -e s=^.==)
+}
+
 # user cdev- functions #########################################################
+
+function cdev-prep()
+{
+    if ! get-image-name; then return 1; fi
+
+    echo "devcontiner name is ${container_name}"
+
+    # if the work image tag does not yet exist then pull the latest one and tag
+    if [ -z $(podman images -q ${image_tag}) ] ; then
+        podman pull ${image_tag_base}:main
+        podman tag ${image_tag_base}:main ${image_tag}
+    fi
+
+    # extract the repos folder so that changes are persisted in the host filesystem
+    echo "copying container /repos to ${repo_root}/repos ..."
+    podman run --rm --privileged -v ${repo_root}/repos:/copy ${image_tag} rsync -a /repos/ /copy
+}
 
 function cdev-launch
 {  
-    if [ -z "${2}" ] ; then 
-        echo 'usage: cdev-launch <image name> <container name> ["podman options"] ["container arguments"]'
-        echo 'note options and args must be quoted'
-        return 1 
+    if ! get-image-name; then return 1; fi
+    
+    if [ "$(podman ps -q -f name=${container_name})" ]; then
+        : # container already running so no prep required
+    elif [ "$(podman ps -qa -f name=${container_name})" ]; then
+        # start the stopped container
+        ( 
+            set -x; 
+            podman start ${container_name}
+        )
+    else
+        # create a new background container making process 1 be 'sleep'
+        ( 
+            set -x; 
+            podman run -d --name ${container_name} ${all_params} ${image_tag} sleep 100d
+        )
     fi
-
-    image="${1}"; name="${2}"; options="${3}"; args="${4}"
-
-    if [ "$(podman ps -qa -f name=${name})" ]; then
-        echo "The container name exists. Please choose a new unique name"
-        echo "or connect to an exisiting container with cdev-connect."
-        return 1
-    elif podman volume exists ${name} ; then
-        echo "WARNING: reconnecting to existing volume ${name}"
-    fi
-
-    # NOTE that all containers must place all source below /repos
-    volume="-v ${name}:/repos"
-
+    # run a shell in the container - this allows multiple shells and avoids using 
+    # process 1 so users can exit the shell without killing the container
     ( 
         set -x; 
-        podman run --name ${name} ${volume} ${all_params} ${options} ${image} ${args}
+        podman exec -it ${container_name} bash
     )
 }
 
-function cdev-connect
-{  
-    if [ -z "${1}" ] ; then 
-        echo "usage: cdev-connect <container name>"; return 1; fi
-
-    name="${1}"; shift 1
-
-    if [ "$(podman ps -q -f name=${name})" ]; then
-        echo "attaching to running container ${1}"
-        ( 
-            set -x; 
-            podman attach ${name} "${@}"
-        )
-    elif [ "$(podman ps -qa -f name=${name})" ]; then
-        echo "launching existing stopped container ${1}"
-        ( 
-            set -x; 
-            podman start --attach ${name} "${@}"
-        )
-    fi
-}
 
 function cdev-launch-ioc()
 {   
-    if [ -z "${2}" ] || [ ! -f "${1}/values.yaml" ] ; then 
-        echo "usage: cdev-launch-ioc <ioc helm chart folder> <container name>"; return 1; fi
+    if [ -z "${1}" ] || [ ! -f "${1}/values.yaml" ] ; then 
+        echo "usage: cdev-launch-ioc <ioc helm chart folder>"; return 1; fi
 
     root="${1}"; name="${2}"; shift 2
 
@@ -89,12 +108,12 @@ function cdev-launch-ioc()
     # get the ioc folder from the values file
     ioc_folder=$(grep iocFolder ${root}/values.yaml | awk '{print $2}')
     # get the most recent local tag for image
-    tag=$(podman images | /bin/grep ${image} -wm 1 | awk '{print $2}')
-    if [ -z "${tag}" ]; then echo "no local image ${image}"; return 1; fi
+    tag=$(podman images | /bin/grep ${image_tag} -wm 1 | awk '{print $2}')
+    if [ -z "${tag}" ]; then echo "no local image ${image_tag}"; return 1; fi
 
     config="-v $(realpath ${root})/config:${ioc_folder}/config"
 
-    cdev-launch ${image}:${tag} ${name} "${config}" "bash ${ioc_folder}/config/start.sh"
+    cdev-launch ${image_tag}:${tag} ${container_name} "${config}" "bash ${ioc_folder}/config/start.sh"
 }
 
 function cdev-debug-last-build()
@@ -105,21 +124,23 @@ function cdev-debug-last-build()
     name="${1}"; shift 1
 
     last_image=$(podman images | awk '{print $3}' | awk 'NR==2')
-    cdev-launch ${last_image} ${name} "${@}"
+    cdev-launch ${last_image} ${container_name} "${@}"
+}
+
+function cdev-stop()
+{
+    if ! get-image-name; then return 1; fi
+
+    # free up resources but keep the container so you can come back to it
+    podman stop ${container_name}
 }
 
 function cdev-rm()
 {
-    if [ -z "${1}" ] ; then 
-        echo "usage: cdev-rm <container name>"; return 1; fi
 
-    name="${1}"; shift 1
+    if ! get-image-name; then return 1; fi
 
-    if [ "$(podman ps -qa -f name=${name})" ]; then 
-        podman container rm ${name}
-    fi
-    if podman volume exists ${name} ; then 
-        podman volume rm ${name}
-    fi
+    # free up resources but keep the container so you can come back to it
+    podman container rm -f ${container_name}
 }
 
